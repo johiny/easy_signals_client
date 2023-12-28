4481625afecad68ec26875810b157c3c44dfa3a9
johiny <johiny7@live.com>
2023-12-28 17:22:43 -0500

    workin on full screeen

--- App.js
+++ App.js
@@ -2,9 +2,14 @@ import ReactNPaperWrapper from './components/ReactNPaperWrapper';
 import Main from './views/Main';
 import * as ScreenOrientation from "expo-screen-orientation";
 import { useEffect } from "react";
+import ImmersiveMode from 'react-native-immersive-mode';
 export default function App() {
   useEffect(() => {
     lockOrientation();
+    ImmersiveMode.fullLayout(true);
+    return () => {
+    ImmersiveMode.fullLayout(false);
+    }
   }, []);
   const lockOrientation = async () => {
     await ScreenOrientation.lockAsync(
